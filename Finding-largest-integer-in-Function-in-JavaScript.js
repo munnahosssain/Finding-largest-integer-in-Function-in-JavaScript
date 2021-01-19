4b042ee44509e11ee4a033c28ed50f427ec6f3e7
Munna Hossain <mayinuddin.munna@gmail.com>
2021-01-19 13:18:05 +0600

    last updated without user value

--- Finding-largest-integer-in-Function-in-JavaScript.js
+++ Finding-largest-integer-in-Function-in-JavaScript.js
@@ -1,1 +1,14 @@
-console.log('Hello');
+var largestNumber, longer,output, num, i;
+function largestNumber(num){
+    longer=num[0];
+    for(i=1; i<=num.length; i++){
+        element=num[i];
+        if (element>longer) {
+            longer=element;
+        }
+    }
+    return longer;
+}
+num =[65, 32, 98, 87, 54, 21, 25, 69, 58, 47, 50, 100];
+output=largestNumber(num);
+console.log('output', output);
